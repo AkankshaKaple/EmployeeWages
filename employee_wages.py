b6504df5ea8e2ee3b6406295a0615025371ee843
Akanksha Kaple <40362921+AkankshaKaple@users.noreply.github.com>
2020-01-10 18:16:15 +0530

    git commit -m "Refactor: UC-9"

--- employee_wages.py
+++ employee_wages.py
@@ -11,6 +11,7 @@ class EmployeeWages:
         daily_wages = []
         hours_worked = 0
         days = 0
+        total_wage = 0
         while True:
             days += 1
             if random.randrange(0, 2) == 0:
@@ -19,35 +20,25 @@ class EmployeeWages:
                 attendance.append('Present')
                 if random.randrange(0, 2) == 0:
                     category.append("Permanent")
-                    hours_worked += 8
+                    total_daily_hours = 8
+                    hours_worked += total_daily_hours
                     daily_wages.append(8 * self.wage_per_hour)
+                    total_wage = total_wage + (self.wage_per_hour * total_daily_hours)
 
                 else:
                     category.append("PartTime")
-                    hours_worked += 4
+                    total_daily_hours = 4
+                    hours_worked += total_daily_hours
                     daily_wages.append(4 * self.wage_per_hour)
+                    total_wage = total_wage + (self.wage_per_hour * total_daily_hours)
 
             if days == 20 or hours_worked == 100:
                 print("Month complete")
-                return attendance, category, days, hours_worked, daily_wages
-
-    def calculate_daily_wage(self, attendance, category):
-        global total_daily_hours
-        wage_per_hour = 20
-        present_days = attendance.count('Present')
-        total_wage = 0
-        for i in range(present_days):
-            if category[i] == "Permanent":
-                total_wage = total_wage + (wage_per_hour * total_daily_hours)
-            elif category[i] == "PartTime":
-                total_daily_hours = 4
-                total_wage = total_wage + (wage_per_hour * total_daily_hours)
-        return total_wage, present_days
+                return attendance, category, days, hours_worked, daily_wages, total_wage, attendance.count("Present")
 
     def show_data(self):
         print("Name : ")
-        attendance, category, working_days, hours_worked, daily_wages = self.employee_attendance()
-        total_wages, present_days = self.calculate_daily_wage(attendance, category)
+        attendance, category, working_days, hours_worked, daily_wages, total_wages, present_days = self.employee_attendance()
         print("Salary for {} days = {}".format(present_days, total_wages))
         print("Employee was present for {} out of {} working days for {} hours.".format(present_days, working_days,
                                                                                         hours_worked))
