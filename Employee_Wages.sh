431fce5dbd1de9353bfcac05c102c4d27863ef54
AkankshaKaple <akankshakaple@gmail.com>
2020-01-15 15:15:25 +0530

    Added: Calculate daily wage of emp

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -1,8 +1,13 @@
 isPresent=1
+WagePerHour=20
+TotalHours=8
+salary=0
 empCheck=$(( RANDOM%2 ))
 if [[ $empCheck -eq $isPresent ]]
 then
 	echo "Present"
+	salary=$(( WagePerHour*TotalHours ))
 else
 	echo "Absent"
 fi
+echo "Salary =" $salary
