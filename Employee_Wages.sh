4547b6dc3ad3d2b52eba624c0f967b6026d63705
AkankshaKaple <akankshakaple@gmail.com>
2020-01-15 16:19:47 +0530

    Wage for 20 days

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -3,18 +3,25 @@ isPartTime=0
 WagePerHour=20
 TotalHours=8
 salary=0
-empCheck=$(( RANDOM%3 ))
-case $empCheck in
-	$isFullTime)
-		TotalHours=8
-		salary=$(( WagePerHour*TotalHours ))
-		;;
-	$isPartTime)
-		TotalHours=1
-		salary=$(( WagePerHour*TotalHours ))
-		;;
-	*)
-		echo "Absent"
-		;;
-esac
-echo "Salary =" $salary
+TotalWorkingDays=20
+
+for (( day=1; day<=TotalWorkingDays; day++ ))
+do
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$isFullTime)
+			TotalHours=8
+			dailyWage=$(( WagePerHour*TotalHours ))
+			;;
+		$isPartTime)
+			TotalHours=4
+			dailyWage=$(( WagePerHour*TotalHours ))
+			;;
+		*)
+			echo "Absent"
+			;;
+	esac
+	salary=$(( salary+dailyWage ))
+done
+echo $salary
+
