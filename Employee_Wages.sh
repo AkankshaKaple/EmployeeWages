91e061c3427a466b63b3390a14297a697892f873
AkankshaKaple <akankshakaple@gmail.com>
2020-01-15 15:38:56 +0530

    Added: Calculate wages for part time and full time

--- Employee_Wages.sh
+++ Employee_Wages.sh
@@ -1,13 +1,20 @@
-isPresent=1
+isFullTime=1
+isPartTime=0
 WagePerHour=20
 TotalHours=8
 salary=0
-empCheck=$(( RANDOM%2 ))
-if [[ $empCheck -eq $isPresent ]]
-then
-	echo "Present"
-	salary=$(( WagePerHour*TotalHours ))
-else
-	echo "Absent"
-fi
+empCheck=$(( RANDOM%3 ))
+case $empCheck in
+	$isFullTime)
+		TotalHours=8
+		salary=$(( WagePerHour*TotalHours ))
+		;;
+	$isPartTime)
+		TotalHours=1
+		salary=$(( WagePerHour*TotalHours ))
+		;;
+	*)
+		echo "Absent"
+		;;
+esac
 echo "Salary =" $salary
